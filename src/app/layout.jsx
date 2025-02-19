6aa484e0de5afb7dc3900f276e1b22cb654da8b3
rodrigo solis <rodrigo.solis.g@gmail.com>
2025-02-19 11:51:11 -0300

    trabajando en los avances de la libreria de componentes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import "../styles/global.scss";
 import "../styles/layouts-styles/main-layout.scss";
 
+import Header from "../components/Header";
+import NavigationDrawer from "../components/NavigationDrawer";
+import MainContent from "../components/MainContent";
+
 export const metadata = {
   title: "Web Component Library",
   description: "estructura base para documentar componentes",
@@ -10,7 +14,11 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        <Header />
+        <NavigationDrawer />
+        <MainContent children={children} />
+      </body>
     </html>
   );
 };
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/pages-styles/main-page-style.scss";
+
+const MainContent = ({ children }) => {
+  return <div className="MainContent">{children}</div>;
+};
+
+export default MainContent;
